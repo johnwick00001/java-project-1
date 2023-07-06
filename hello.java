32e381ce5ca9a0bbc1045cf622f82ec04390e17a
Cloud User <ec2-user@ip-172-31-89-49.ec2.internal>
2023-07-06 23:59:15 +0000

    all files added

---
+++ hello.java
@@ -0,0 +1,14 @@
+// hello world
+public class hello {
+
+
+	//the main method
+	public static void main( String[] args ) {
+
+
+		// print Hello World
+		System.out.println( "Hello World !!" );
+
+	}
+
+}
